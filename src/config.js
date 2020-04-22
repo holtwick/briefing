3dddcad3ba4fadbf2c78686735e2d64226f03b4e
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-04-22 16:51:31 +0200

    Set new servers

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 // See https://github.com/holtwick/peer2school-server
-export const SIGNAL_SERVER_URL = 'wss://signal.brie.fi'
+export const SIGNAL_SERVER_URL = 'wss://sig03.brie.fi'
 
 // See https://github.com/feross/simple-peer#peer--new-peeropts
 export const ICE_CONFIG = {
   iceTransportPolicy: 'all',
   reconnectTimer: 3000,
   iceServers: [{
-    urls: 'stun:stun.brie.fi:5349',
+    urls: 'stun:turn01.brie.fi:5349',
   }, {
-    urls: 'turn:stun.brie.fi:5349',
+    urls: 'turn:turn01.brie.fi:5349',
     username: 'brie',
     credential: 'fi',
   }],
