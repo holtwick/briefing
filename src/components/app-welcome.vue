23cf77ca16e8940712b976e29f0f3cb86b46292d
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-04-27 17:09:00 +0200

    Start styling

--- src/components/app-welcome.vue
+++ src/components/app-welcome.vue
@@ -1,5 +1,4 @@
 <template>
-  <div>
     <div class="page1">
       <div class="logo">
         <form id="form">
@@ -12,7 +11,7 @@
         </div>
       </div>
       <div class="footer links">
-        <p>End-to-end encrypted group video chat. <a href="#help" onclick="document.getElementById('help').scrollIntoView({behavior: 'smooth'}); return false;">Help</a></p>
+        <p>Anonymous end-to-end encrypted group video chat.
         <p>Created by <a href="https://holtwick.de?ref=briefing">holtwick.de</a>
           <a title="Twitter" href="https://twitter.com/holtwick" class="icon">
             <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title>
@@ -41,83 +40,19 @@
         </p>
       </div>
     </div>
-    <div class="help" id="help">
-      <div class="text links">
 
-        <h1>Brie.fi/ng</h1>
-        <p><strong>Welcome to Briefing. Yet another secure video chat.</strong></p>
-        <p>Privacy is the driving force behind this project. It uses secure technologies like <a href='https://webrtc-security.github.io/'>WebRTC</a> to directly connect between participants. The website that provides the web app and
-          mediates
-          the communication stores as few data as possible and does not know anything about the contents of established conversations. No accounts are required. No cookies are used.</p>
-        <p>The difference between Briefing and most similar projects is, that it does not use a central server that distributes the video streams (<a href='https://webrtcglossary.com/sfu/'>SFU</a>). The advantage of an SFU is that it saves
-          bandwidth due to the fact that the own video does not being uploaded to each participant but only once. The SFU can also do more optimizations the clients might not support. But then the video signal is not end-to-end encrypted
-          any
-          more i.e. you have to trust the SFU provider. Briefing instead sends data from peer to peer directly (&quot;Mesh&quot;) and is therefore end-to-end encrypted by default by WebRTC implementations.</p>
-        <p><strong>FAQ- Some answers to some questions</strong></p>
-        <p><em>Why starting another video conf tool?</em></p>
-        <p>During the Corona pandemic I tried to create a peer-to-peer solution for home schooling called <a href='https://peer.school/?referrer=peer.school'>peer.school</a>. Although the project has been retired for various reasons, I
-          learned
-          so much about WebRTC and video/audio communication, that I did want to reuse this knowledge in this project. </p>
-        <p><em>Why is it free and Open Source?</em></p>
-        <p>Only a free project does not require the user to create an account and unveil payment information, that could be used to identify that person. This is why the project is free and will remain free, as long as no additional costs
-          like
-          maintaining TURN servers etc. will require some funding. If you want to support me and the project directly you can <a href='https://www.paypal.me/apperdeck'>via PayPal</a>. If you are interested in sponsoring please contact me at
-          <a
-            href='mailto:briefing@holtwick.de' target='_blank' class='url'>briefing@holtwick.de</a>.</p>
-        <p><em>Which servers are actually involved in a call?</em></p>
-        <ul>
-          <li>Briefing web site (provides the web app)</li>
-          <li>Briefing signaling server (coordinates peers in rooms)</li>
-          <li>Briefing STUN/TURN server (helps traversing fire walls)</li>
-        </ul>
-        <p><em>Feature X is missing! Found bug Y!</em></p>
-        <p>Please visit the <a href='https://github.com/holtwick/briefing/issues'>GitHub project</a> to see if your request is already on the list or place a <a href='https://github.com/holtwick/briefing/issues/new'>new issue</a>. Your
-          contribution is of course welcome as well.</p>
-        <p><em>How about native apps?</em></p>
-        <p>Frankly this is not required, because your browser can handle it all. You can of course put Briefing on your home screen e.g. in iOS Safari tap on the &quot;Share&quot; icon and choose &quot;Add to Home Screen&quot;. You can do
-          the
-          same in browsers like Google Chrome</p>
-
-      </div>
-    </div>
-  </div>
 </template>
 
 <style lang="scss">
-html {
-  -webkit-tap-highlight-color: transparent;
-}
 
-html body * {
-  box-sizing: border-box;
-  -webkit-text-size-adjust: none;
-}
-
-html *:not(input,textarea,[contenteditable]) {
-  user-select: none;
-  -webkit-touch-callout: none;
-}
-
-html, body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  text-align: center;
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, Segoe UI, Roboto, Hiragino Sans, Hiragino Kaku Gothic Pro, Yu Gothic, YuGothic, Meiryo, Helvetica Neue, sans-serif;
-  background: #272727;
-  color: #eee;
-  display: flex;
-  flex-direction: column;
-}
 
 .page1 {
+  text-align: center;
   flex-shrink: 0;
   height: 100%;
   display: flex;
   flex-direction: column;
+  line-height: 1.5;
 }
 
 .help {
