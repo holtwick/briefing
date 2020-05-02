8da56f739ffb2c2d00369dcd1f46718ccb8cc6e1
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-05-02 16:34:04 +0200

    Translate

--- src/components/app-main.vue
+++ src/components/app-main.vue
@@ -20,14 +20,7 @@
       />
 
       <div class="message-container" v-if="!hasPeers">
-        <div class="message">
-          Invite by sending the link via pressing on the
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path>
-            <polyline points="16 6 12 2 8 6"></polyline>
-            <line x1="12" y1="2" x2="12" y2="15"></line>
-          </svg>
-          symbol.
+        <div class="message" v-html="l.share.message">
         </div>
       </div>
 
--- src/locales/en.js
+++ src/locales/en.js
@@ -5,6 +5,13 @@ export default {
     button_copy: 'Copy',
     qr_info: 'You can also scan this QR Code with a mobile device camera:',
     feedback: 'For feedback write to <a href="mailto:support@holtwick.de?subject=Briefing">support@holtwick.de</a>',
+    message: 'Invite by sending the link via pressing on the\n' +
+      '          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">\n' +
+      '            <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path>\n' +
+      '            <polyline points="16 6 12 2 8 6"></polyline>\n' +
+      '            <line x1="12" y1="2" x2="12" y2="15"></line>\n' +
+      '          </svg>\n' +
+      '          symbol.',
   },
   settings: {
     title: 'Settings',
@@ -13,6 +20,6 @@ export default {
     desktop: 'Share screen or window',
     blur: 'Blur background',
     bandwidth: 'Apply bandwidth optimizations',
-    fill: 'Scale up video'
-  }
+    fill: 'Scale up video',
+  },
 }
