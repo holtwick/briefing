0ce4da10ee40291b63c75c52b7d861504957f9a2
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-05-05 20:56:30 +0200

    External links fail

--- src/pwa.js
+++ src/pwa.js
@@ -1,25 +1,54 @@
 import Vue from 'vue'
-import App from './pwa-app.vue'
 import locale from './lib/locale'
+import './lib/registerServiceWorker'
 import de from './locales/de'
 import en from './locales/en'
+import App from './pwa-app.vue'
 import { state } from './state'
-import './lib/registerServiceWorker'
 
-var userAgent = navigator.userAgent.toLowerCase();
-if (userAgent.indexOf(' electron/') > -1) {
-  const customTitlebar = require('custom-electron-titlebar');
-  console.info("Runs Electron")
- 
-  new customTitlebar.Titlebar({
-      backgroundColor: customTitlebar.Color.fromHex('#272727')
-  });
+// Electron specific
+if (navigator.userAgent.toLowerCase().indexOf(' electron/') > -1) {
+  console.info('Runs Electron')
+  document.title = 'Briefings'
+
+  // https://www.npmjs.com/package/custom-electron-titlebar
+  import('custom-electron-titlebar').then(customTitlebar => {
+    new customTitlebar.Titlebar({
+      backgroundColor: customTitlebar.Color.fromHex('#272727'),
+    })
+  })
+
+  import('electron').then(electron => window.electron = electron)
 }
+
 Vue.config.productionTip = false
 
 Vue.mixin({
   data() {
     return { state }
+  },
+  methods: {
+    openExternalLink(event) {
+      if (window.electron) {
+        let href
+        if (typeof event === 'string') {
+          href = event
+        } else {
+          let target = event?.target
+          while (target && target?.href == null) {
+            target = target.parentElement
+          }
+          href = target?.href
+        }
+        console.info('Open external link', event.target)
+        if (href) {
+          window.electron.shell.openExternal(href)
+        }
+        event.preventDefault()
+        return false
+      }
+      return true
+    },
   },
 })
 
