dee4d6abd8756b06dcc2abbbb26279b0884d33d8
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-06-14 12:21:26 +0200

    Fix blur errors

--- app/src/logic/blur.js
+++ app/src/logic/blur.js
@@ -46,10 +46,16 @@ function setVideoStream(videoEl, stream) {
 
   // https://github.com/tensorflow/tfjs-models/blob/b72c10bdbdec6b04a13f780180ed904736fa52a5/body-pix/demos/index.js#L117
   return new Promise((resolve) => {
-    videoEl.onloadedmetadata = () => {
+    // videoEl.onloadedmetadata = () => {
+    //   videoEl.width = videoEl.videoWidth
+    //   videoEl.height = videoEl.videoHeight
+    //   videoEl.onloadedmetadata = undefined
+    //   resolve(videoEl)
+    // }
+    videoEl.onloadeddata = () => {
       videoEl.width = videoEl.videoWidth
       videoEl.height = videoEl.videoHeight
-      videoEl.onloadedmetadata = undefined
+      videoEl.onloadeddata = undefined
       resolve(videoEl)
     }
   })
@@ -94,9 +100,9 @@ export async function startBlurTransform(stream) {
       assert(outputEl, 'expect videoEl')
 
       await setVideoStream(videoEl, stream)
-      await startTransformer(videoEl, outputEl)
-
       await videoEl.play()
+
+      await startTransformer(videoEl, outputEl)
 
       if (!captureStream) {
         captureStream = outputEl.captureStream()
