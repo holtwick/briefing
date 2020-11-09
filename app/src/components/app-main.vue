8765b70c783aea424b869783d9db7bec92401a02
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-11-09 22:04:46 +0100

    Standardized external links

--- app/src/components/app-main.vue
+++ app/src/components/app-main.vue
@@ -115,9 +115,10 @@
             </svg>
           </sea-link>
         </div>
-        <sea-link v-if="supportsFullscreen" @action="doToggleFullScreen" class="tool" :class="{'-active': mode === 'share'}">
+        <sea-link v-if="supportsFullscreen" @action="doToggleFullScreen" class="tool">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" v-if="!isFullScreen">
+               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+               v-if="!isFullScreen">
             <path
                 d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path>
           </svg>
@@ -192,7 +193,7 @@ export default {
       dragOver: false,
       supportsFullscreen: document.fullscreenEnabled,
       isFullScreen: false,
-      fullscreenHandler: null
+      fullscreenHandler: null,
     }
   },
   computed: {
@@ -272,7 +273,7 @@ export default {
     },
     didChangeFullscreen(ev) {
 
-    }
+    },
   },
   async mounted() {
     this.conn = await setup()
