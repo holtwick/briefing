437eac561238f0141d7c72ae6d97a4b1e8760628
Dirk Holtwick <dirk.holtwick@gmail.com>
2022-09-28 15:42:33 +0200

    More defaults

--- vite/src/config.js
+++ vite/src/config.js
@@ -13,12 +13,12 @@ export const ICE_CONFIG = {
   // These settings are no secret, since they are readable from the client side anyway
   iceServers: [
     {
-      urls: import.meta.env.BRIEFING_STUN_URL,
+      urls: import.meta.env.BRIEFING_STUN_URL ?? "stun:turn01.brie.fi:5349",
     },
     {
-      urls: import.meta.env.BRIEFING_TURN_URL,
-      username: import.meta.env.BRIEFING_TURN_USER,
-      credential: import.meta.env.BRIEFING_TURN_PASSWORD,
+      urls: import.meta.env.BRIEFING_TURN_URL ?? "turn:turn01.brie.fi:5349",
+      username: import.meta.env.BRIEFING_TURN_USER ?? "brie",
+      credential: import.meta.env.BRIEFING_TURN_PASSWORD ?? "fi",
     },
   ],
 }
@@ -31,8 +31,7 @@ export const RELEASE = import.meta.env.BRIEFING_RELEASE
 export const SENTRY_DSN = import.meta.env.BRIEFING_SENTRY_DSN
 
 export const ROOM_URL =
-  import.meta.env.BRIEFING_ROOM_URL ||
-  `${location.protocol}://${location.host}/` // "https://brie.fi/ng/"
+  import.meta.env.BRIEFING_ROOM_URL || `${location.protocol}//${location.host}/` // "https://brie.fi/ng/"
 export const ROOM_PATH = import.meta.env.BRIEFING_ROOM_PATH || "/" // "/ng/"
 
 export const SHOW_FULLSCREEN = isTrue(
