5336e03e82fedecee5692fcba01c831abfd75460
Dirk Holtwick <dirk.holtwick@gmail.com>
2022-10-10 11:53:47 +0200

    Fix the stream setting issue

--- src/logic/webrtc-peer.js
+++ src/logic/webrtc-peer.js
@@ -113,28 +113,11 @@ export class WebRTCPeer extends Emitter {
   }
 
   setStream(stream) {
-    if (!this.peer.streams.includes(stream)) {
-      try {
-        this.peer.streams.forEach((s) => {
-          try {
-            this.peer.removeStream(s)
-          }
-          catch (err) {
-            trackException(err)
-          }
-        })
-      }
-      catch (err) {
-        trackException(err)
-      }
-      if (stream) {
-        try {
-          this.peer.addStream(stream)
-        }
-        catch (err) {
-          trackException(err)
-        }
-      }
+    try {
+      this.peer.setStream(stream)
+    }
+    catch (err) {
+      trackException(err)
     }
   }
 
