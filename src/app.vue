dae9042c72aaa75f66080d3a3af8e162b2646ed2
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-04-04 00:19:36 +0200

    small fixes for banner

--- src/app.vue
+++ src/app.vue
@@ -22,7 +22,7 @@ const showBanner = useLocalStorage('_briefingBetaBanner', true)
     <div class="banner-close" @click="showBanner = false">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
     </div>
-    <a href="https://beta.brie.fi/ng?ref=banner">Would you like to try the latest version 4 of Briefing? Visit the beta at https://beta.brie.fi</a>
+    <a href="https://beta.brie.fi/ng?ref=banner" target="_blank">Try the new beta of Briefing 4.0 at https://beta.brie.fi</a>
   </div>
   <AppEmbed v-if="state.embedDemo === true" class="app" />
   <AppWelcome v-else-if="!state.room && state.original" class="app" />
